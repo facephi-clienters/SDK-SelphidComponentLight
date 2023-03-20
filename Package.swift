aa2f1fb2ec4a45c0f20fcf5b492f89035ca26759
Jorge Poveda <jpoveda@facephi.com>
2023-03-20 18:41:20 +0100

    [AM-439]: release with selphidComponentLight.xcframework in version  1.1.1. This framework has been compiled and generated using cocoapods build_workspace.sh script

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "selphidComponentLight",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "selphidComponentLight",      
+            targets: ["selphidComponent"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "git@github.com:facephi-clienters/selphidnoml-framework.git", from:"1.18.0"),
+        .package(url: "git@github.com:facephi-clienters/SDK-CorePackage-SPM.git", from: "1.1.5"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "selphidComponent",
+            dependencies: [
+                .product(name: "FPhiSelphIDWidgetiOSLight", package: "selphidnoml-framework"),
+                "SDK-CorePackage-SPM",
+                "selphidComponentLight"
+            ]
+        ),
+        .binaryTarget(name: "selphidComponentLight", path: "selphidComponentLight.xcframework")
+    ]
+)
---
+++ Sources/selphidComponent/SelphidComponentLight.swift
@@ -0,0 +1,10 @@
+//
+//  SelphidComponentLight.swift
+//  
+//
+//  Created by Jorge Poveda on 16/3/23.
+//
+
+import selphidComponentLight
+
+class SelphidComponentLight {}
