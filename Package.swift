438404a83ed32ee8c2cf20d4e0a69a91db109936
Facephi <developer@facephi.com>
2023-08-04 13:13:20 +0200

    New SPM Version: 1.4.2

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "git@github.com:facephi-clienters/selphidnoml-framework.git", from: "1.19.2"),
+        .package(url: "git@github.com:facephi-clienters/selphidnoml-framework.git", from: "1.20.0"),
         .package(url: "git@github.com:facephi-clienters/SDK-CorePackage-SPM.git", from: "1.4.2"),
     ],
     targets: [
