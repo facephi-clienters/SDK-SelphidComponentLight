c2efe82404230ce19536000f3cffdfc6a323b03f
Facephi <developer@facephi.com>
2025-07-02 14:46:21 +0200

    New SPM Version: 2.3.11

--- Package.swift
+++ Package.swift
@@ -4,31 +4,31 @@
 import PackageDescription
 
 let package = Package(
-    name: "selphidComponentLight",
+    name: "selphidComponent",
     defaultLocalization: "es",
     platforms: [.iOS(.v9)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "selphidComponentLight",
-            targets: ["selphidComponent"]),
+            name: "selphidComponent",
+            targets: ["selphidLightComponent", "selphidComponent"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "git@github.com:facephi-clienters/selphidnoml-framework.git", .upToNextMinor(from: "1.33.1")),
+        .package(url: "git@github.com:facephi-clienters/selphidnoml-framework.git", .upToNextMinor(from: "1.33.3")),
         .package(url: "git@github.com:facephi-clienters/SDK-CorePackage-SPM.git", .exactItem("2.3.2")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "selphidComponent",
+            name: "selphidLightComponent",
             dependencies: [
                 .product(name: "FPhiSelphIDWidgetiOSLight", package: "selphidnoml-framework"),
                 "SDK-CorePackage-SPM",
-                "selphidComponentLight"
+                "selphidComponent"
             ],
             resources: [.process("Resources")]),
-        .binaryTarget(name: "selphidComponentLight", path: "selphidComponentLight.xcframework")
+        .binaryTarget(name: "selphidComponent", path: "selphidComponent.xcframework")
     ]
 )
