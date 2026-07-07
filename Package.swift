9f0030101e43880ade98bfe593e36362bc9f446f
Facephi <developer@facephi.com>
2026-07-07 19:09:57 +0200

    New SPM Version: 2.9.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/facephi-clienters/selphidnoml-framework.git", .upToNextMinor(from: "1.39.2")),
-        .package(url: "https://github.com/facephi-clienters/SDK-CorePackage-SPM.git", .exactItem("2.8.1")),
+        .package(url: "https://github.com/facephi-clienters/selphidnoml-framework.git", .upToNextMinor(from: "1.39.3")),
+        .package(url: "https://github.com/facephi-clienters/SDK-CorePackage-SPM.git", .exactItem("2.9.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -30,7 +30,7 @@ let package = Package(
             ],
             resources: [.process("Resources")]),
         .binaryTarget(name: "selphidComponent",
-        url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/SDK/FPHISDKSelphIDComponentLight/2.8.2/selphidComponent.zip",
-        checksum: "b9fd61315f9807d4f560f1f475a81e5607cf43e9517cbc0c30f71a1936f83d34")
+        url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/SDK/FPHISDKSelphIDComponentLight/2.9.0/selphidComponent.zip",
+        checksum: "94c93c4dec794d09a2819f82d1d95d5ff5de4f85b013d9c8d5b01cf492ac54bc")
     ]
 )
